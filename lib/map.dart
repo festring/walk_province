e0760f9d90f9594ea36ca16c8eb52a155e8ac94a
Rio Choi <architekt140@gmail.com>
2024-11-06 21:40:09 +0900

    map 분리

---
+++ lib/map.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapSample extends StatefulWidget {
+  const MapSample({super.key});
+
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(37.7749, -122.4194); // 샌프란시스코 위치 예시
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('검색창 들어갈 곳'),
+        backgroundColor: const Color.fromARGB(255, 201, 239, 203),
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 11.0,
+        ),
+      ),
+    );
+  }
+}
