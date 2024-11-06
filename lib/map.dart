255902d54ad9c9644330b4be3feb20b5a25d84ae
Rio Choi <architekt140@gmail.com>
2024-11-06 22:49:21 +0900

    현재위치로 지도 자동이동

--- lib/map.dart
+++ lib/map.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class MapSample extends StatefulWidget {
   const MapSample({super.key});
@@ -9,27 +11,62 @@ class MapSample extends StatefulWidget {
 }
 
 class MapSampleState extends State<MapSample> {
-  late GoogleMapController mapController;
+  GoogleMapController? _mapController;
 
-  final LatLng _center = const LatLng(37.7749, -122.4194); // 샌프란시스코 위치 예시
+  LatLng _currentPosition = LatLng(37.7749, -122.4194); // 샌프란시스코
 
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+  @override
+  void initState() {
+    super.initState();
+    _checkPermissionAndSetInitialLocation();
+  }
+
+  // 초기 권한 체크 및 위치 설정
+  Future<void> _checkPermissionAndSetInitialLocation() async {
+    if (await _requestPermission()) {
+      _setCurrentLocation();
+    } else {
+      print('위치 권한이 거부되었습니다.');
+    }
+  }
+
+  // 버튼 클릭 시 위치 설정
+  Future<void> _setCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition();
+      _currentPosition = LatLng(position.latitude, position.longitude);
+      print(position);
+
+      // 지도 카메라 이동
+      _mapController?.animateCamera(
+        CameraUpdate.newLatLng(_currentPosition),
+      );
+    } catch (e) {
+      print('현재 위치를 가져오는 중 오류 발생: $e');
+    }
+  }
+
+  Future<bool> _requestPermission() async {
+    PermissionStatus status = await Permission.location.request();
+    return status.isGranted;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('검색창 들어갈 곳'),
-        backgroundColor: const Color.fromARGB(255, 201, 239, 203),
-      ),
+      appBar: AppBar(title: const Text('Google Maps')),
       body: GoogleMap(
-        onMapCreated: _onMapCreated,
+        onMapCreated: (controller) => _mapController = controller,
         initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: 11.0,
+          target: _currentPosition,
+          zoom: 14.0,
         ),
+        myLocationEnabled: true,
+        myLocationButtonEnabled: false, // 기본 위치 버튼 비활성화
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _setCurrentLocation,
+        child: Icon(Icons.my_location),
       ),
     );
   }
