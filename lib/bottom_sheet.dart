ec40a55c346743710497df376a21f9f11e2b035a
Rio Choi <architekt140@gmail.com>
2024-11-10 22:30:39 +0900

    bottomSheet가 화면 위까지 드래그됨

--- lib/bottom_sheet.dart
+++ lib/bottom_sheet.dart
@@ -3,39 +3,45 @@ import 'package:flutter/material.dart';
 void showCustomBottomSheet(BuildContext context, Map<String, String> item) {
   showModalBottomSheet(
     context: context,
-    isScrollControlled: true, // BottomSheet 크기 조정 가능하도록
-    backgroundColor: Colors.transparent, // 배경을 투명하게 설정
+    isScrollControlled: true,
+    backgroundColor: Colors.transparent,
     builder: (BuildContext context) {
-      return Container(
-        width: double.infinity, // 화면의 너비를 꽉 채움
-        height: MediaQuery.of(context).size.height * 0.5,
-        padding: EdgeInsets.all(16.0),
-        decoration: BoxDecoration(
-          color: Colors.white, // BottomSheet의 배경색
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(16.0),
-            topRight: Radius.circular(16.0),
-          ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              item['Name'] ?? 'No Name',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+      return DraggableScrollableSheet(
+        initialChildSize: 0.5, // BottomSheet가 시작될 때 화면의 50% 크기
+        minChildSize: 0.3, // 최소 크기 (30%)
+        maxChildSize: 1.0, // 최대 크기 (100%)
+        builder: (BuildContext context, ScrollController scrollController) {
+          return Container(
+            width: double.infinity, // 화면의 너비를 꽉 채움
+            padding: EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: Colors.white, // BottomSheet의 배경색
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(16.0),
+                topRight: Radius.circular(16.0),
+              ),
             ),
-            SizedBox(height: 8),
-            Text(
-              item['Description'] ?? 'No Description',
-              style: TextStyle(fontSize: 16),
+            child: ListView(
+              controller: scrollController,
+              children: [
+                Text(
+                  item['Name'] ?? 'No Name',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  item['Description'] ?? 'No Description',
+                  style: TextStyle(fontSize: 16),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  item['Distance'] ?? 'No Distance',
+                  style: TextStyle(fontSize: 14, color: Colors.blueGrey),
+                ),
+              ],
             ),
-            SizedBox(height: 8),
-            Text(
-              item['Distance'] ?? 'No Distance',
-              style: TextStyle(fontSize: 14, color: Colors.blueGrey),
-            ),
-          ],
-        ),
+          );
+        },
       );
     },
   );
